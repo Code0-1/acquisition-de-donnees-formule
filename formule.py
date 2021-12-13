3dd852be0bd6d2a7366a3be1ed6d20a409ec76c3
Simon Durand <simon.durand.5@ulaval.ca>
2021-12-13 14:21:16 -0500

    make_callable sorted id

--- formule.py
+++ formule.py
@@ -5,51 +5,41 @@ def make_callable(idd):
     raise : ValueError('idd non valide')
     return : liste_formules(list)
     '''
-    if idd == 'id0000000A':
-        pass
-    elif idd == 'id00000004':
-        pass
-    elif idd == 'id08020080':
-        pass
-    elif idd == 'id01800000':
-        pass
-    elif idd == 'id01310000':
-        pass
-    elif idd == 'id00000099':
-        pass
-    elif idd == 'id08000080':
-        pass
-    elif idd == 'id02020180':
-        pass
-    elif idd == 'id08010080':
-        pass
-    elif idd == 'id0000000C':
+    if idd == 'id00000004':
         pass
     elif idd == 'id00000008':
         pass
+    elif idd == 'id0000000A':
+        pass
     elif idd == 'id0000000B':
+        pass
+    elif idd == 'id0000000C':
         pass
     elif idd == 'id0000000D':
         pass
+    elif idd == 'id00000099':
+        pass
+    elif idd == 'id01310000':
+        pass
+    elif idd == 'id01800000':
+        pass
+    elif idd == 'id02010001':
+        pass
+    elif idd == 'id02020080':
+        pass
+    elif idd == 'id02020180':
+        pass
     elif idd == 'id02020280':
         pass
-    elif idd == 'id020C0281':
+    elif idd == 'id02020380':
         pass
     elif idd == 'id02020480':
         pass
     elif idd == 'id02020580':
         pass
-    elif idd == 'id02030980':
-        pass
-    elif idd == 'id02010001':
-        pass
-    elif idd == 'id02070081':
-        pass
     elif idd == 'id02020680':
         pass
     elif idd == 'id02020780':
-        pass
-    elif idd == 'id02030C80':
         pass
     elif idd == 'id02020880':
         pass
@@ -57,65 +47,69 @@ def make_callable(idd):
         pass
     elif idd == 'id02020A80':
         pass
-    elif idd == 'id02030E80':
-        pass
     elif idd == 'id02020B80':
         pass
     elif idd == 'id02020C81':
         pass
-    elif idd == 'id02020080':
-        pass
-    elif idd == 'id02031180':
-        pass
     elif idd == 'id02030080':
-        pass
-    elif idd == 'id02020380':
-        pass
-    elif idd == 'id02030580':
-        pass
-    elif idd == 'id02030880':
-        pass
-    elif idd == 'id020C0080':
-        pass
-    elif idd == 'id02031080':
-        pass
-    elif idd == 'id02031381':
-        pass
-    elif idd == 'id020C0180':
-        pass
-    elif idd == 'id02030780':
-        pass
-    elif idd == 'id02030A80':
         pass
     elif idd == 'id02030180':
         pass
-    elif idd == 'id02030480':
-        pass
-    elif idd == 'id02030F80':
+    elif idd == 'id02030280':
         pass
     elif idd == 'id02030380':
         pass
+    elif idd == 'id02030480':
+        pass
+    elif idd == 'id02030580':
+        pass
     elif idd == 'id02030680':
+        pass
+    elif idd == 'id02030780':
+        pass
+    elif idd == 'id02030880':
+        pass
+    elif idd == 'id02030980':
+        pass
+    elif idd == 'id02030A80':
         pass
     elif idd == 'id02030B80':
         pass
-    elif idd == 'id02030280':
+    elif idd == 'id02030C80':
         pass
     elif idd == 'id02030D80':
         pass
+    elif idd == 'id02030E80':
+        pass
+    elif idd == 'id02030F80':
+        pass
+    elif idd == 'id02031080':
+        pass
+    elif idd == 'id02031180':
+        pass
     elif idd == 'id02031280':
+        pass
+    elif idd == 'id02031381':
+        pass
+    elif idd == 'id02070081':
+        pass
+    elif idd == 'id02090080':
         pass
     elif idd == 'id02090181':
         pass
-    elif idd == 'id04010000':
+    elif idd == 'id020C0080':
         pass
-    elif idd == 'id04030000':
+    elif idd == 'id020C0180':
+        pass
+    elif idd == 'id020C0281':
+        pass
+    elif idd == 'id04010000':
         pass
     elif idd == 'id04020000':
         pass
-    elif idd == 'id04040000':
+    elif idd == 'id04030000':
         pass
-    elif idd == 'id02090080':
+    elif idd == 'id04040000':
         pass
     elif idd == 'id04110000':
         pass
@@ -125,19 +119,26 @@ def make_callable(idd):
         pass
     elif idd == 'id04140000':
         pass
-    elif idd == 'id04830000':
-        pass
-    elif idd == 'id04630000':
-        pass
-    elif idd == 'id04530000':
-        pass
     elif idd == 'id04430000':
-        pass
-    elif idd == 'id04840000':
-        pass
-    elif idd == 'id04640000':
-        pass
-    elif idd == 'id04540000':
         pass
     elif idd == 'id04440000':
         pass
+    elif idd == 'id04530000':
+        pass
+    elif idd == 'id04540000':
+        pass
+    elif idd == 'id04630000':
+        pass
+    elif idd == 'id04640000':
+        pass
+    elif idd == 'id04830000':
+        pass
+    elif idd == 'id04840000':
+        pass
+    elif idd == 'id08000080':
+        pass
+    elif idd == 'id08010080':
+        pass
+    elif idd == 'id08020080':
+        pass
+    raise ValueError('idd non valide')
