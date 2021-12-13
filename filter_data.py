fccb5de5b201c7fc6a97c185add8bb94643e9085
Simon Durand <simon.durand.5@ulaval.ca>
2021-12-13 13:57:25 -0500

    renommer filter.py + début main loop

--- filter_data.py
+++ filter_data.py
@@ -5,13 +5,11 @@ import sys
 import json
 
 
-def file_to_data(path):
-    '''filtre les données selon leur identifiant,
-    il ne prend pas en contre la provenance ('can1')
+def get_file(path):
+    '''demande le fichier à filtrer
     args : path
     raise : aucun
-    return : data(dict)'''
-
+    return : nom_du_fichier(str)'''
     files_names = []
     for file_name in os.listdir(path+'data_file\\'):
         if file_name[-4:] == ".txt":
@@ -29,7 +27,14 @@ def file_to_data(path):
         if file_to_filter in files_names:
             break
         print('fichier invalide...')
+    return file_to_filter
 
+def file_to_data(path, file_to_filter):
+    '''filtre les données selon leur identifiant,
+    il ne prend pas en contre la provenance ('can1')
+    args : path, file_to_filter(str et fini par .txt)
+    raise : aucun
+    return : tup(file_name(str), data(dict))'''
     print('Traitement des données...')
     txt = open(path+'data_file\\'+file_to_filter, 'r').read()
     raw_data = txt.split('\n')
@@ -52,9 +57,28 @@ def file_to_data(path):
     return data
 
 def data_to_json(file_name, data):
+    '''écrit les datas dans un json
+    args : file_name(str), data(dict)
+    raise : aucun
+    return : aucun'''
     with open(create_file_name_json(file_name), 'w') as fich:
         fich.write(json.dumps(data))
 
 def create_file_name_json(file_name):
+    '''prend le nom du fichier et créer un fichier file_name+"_json.txt"
+    args : file_name(str)
+    raise : aucun
+    return : nouveau_nom_du _fichier(str)'''
     file_name = file_name[:-4] + '_json.txt'
     return file_name
+
+def read_json(file_name):
+    '''lecture du fichier json
+    args : file_name(str)
+    raise : aucun
+    return : data(dict)'''
+    data = {}
+    with open(create_file_name_json(file_name), 'w') as fich:
+        data = json.loads(fich.read())
+    return data
+
--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+'''fichier avec main loop'''
+from json.decoder import JSONDecoder
+import json
 import os
-
+import filter_data
 
 path = os.path.abspath('main.py')[:-7]
+
+# demande le fichier à filtrer
+file = filter_data.get_file(path)
+
+data = {}
+# regarde si le fichier est déjà filtrer
+'''peu importe l'erreur il va :
+refiltrer tous les données du fichier'''
+try:
+    with open(path+file, 'r') as fich:
+        data = json.loads(fich.read())
+except:
+    data = filter_data.file_to_data(path, file)
+
